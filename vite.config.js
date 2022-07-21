060d73c815054075b1d26a60ebd205afe88afde9
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2022-07-21 19:31:57 +0200

    Update vite config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
+  base: '/train-own-react/',
   esbuild: {
     jsxFactory: 'Fr.el',
   },
