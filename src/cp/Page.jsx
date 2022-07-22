5034b3b25ab3032ea3a344cba6ebbae14238f397
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2022-07-22 13:05:13 +0200

    remove unused files

--- src/cp/Page.jsx
+++ src/cp/Page.jsx
@@ -1,6 +1,3 @@
-import { Input } from './Input'
-import { Links } from './Links'
-
 export const Page = (props, childs) => {
   return (
     <div class="min-h-screen min-w-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black flex flex-col space-y-10 items-center ">
